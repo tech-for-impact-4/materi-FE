d653fe6cee5830d9e5ed8576c078964a7e7f8ac4
auzanassdq <auzanassidqi@gmail.com>
2023-05-08 13:10:11 +0700

    initial commit

---
+++ belajar-react/src/component/Footer.jsx
@@ -0,0 +1,11 @@
+import "./Footer.css"
+
+function Footer() {
+  return (
+    <footer>
+      <span>By Auzan</span>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ belajar-react/src/component/Navbar.jsx
@@ -0,0 +1,16 @@
+function Navbar() {
+  return (
+    <header style={{display: "flex", backgroundColor: "antiquewhite"}}>
+      <h1 className="">Zan</h1>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Navbar
