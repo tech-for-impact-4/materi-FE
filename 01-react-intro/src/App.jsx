6ae62952791cdbd59fc9b9cce6c1728f3e9d4fea
auzanassdq <auzanassidqi@gmail.com>
2023-05-08 13:11:52 +0700

    intro to react

---
+++ 01-react-intro/src/App.jsx
@@ -0,0 +1,22 @@
+import Home from "./Home";
+import Footer from "./component/Footer";
+import Navbar from "./component/Navbar";
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+
+      <main>
+        <section></section>
+        <section></section>
+        <section></section>
+      </main>
+
+      <Footer />
+    </>
+  );
+}
+
+export default App;
